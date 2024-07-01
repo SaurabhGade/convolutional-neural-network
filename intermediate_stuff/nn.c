51c87612ff6fbb0ca788dd870dfab3ee8b8a471a
saurabh <saurabh@debian>
2024-07-01 23:33:10 +0530

    almost complete

--- intermediate_stuff/nn.c
+++ intermediate_stuff/nn.c
@@ -1,13 +1,54 @@
 #define NN_IMPLEMENTATION 
 #include "nn.h"
 #include<time.h>
+#include<math.h>
+
+typedef struct{
+  
+  Mat a0;
+
+  Mat w1, b1, a1;
+
+  Mat w2, b2, a2; 
+} Xor;
+
+
+float forward_xor(Xor m, float x1 , float x2){
+ 
+    MAT_AT(m.a0, 0, 0) = x1;
+    MAT_AT(m.a0, 0, 1) = x2;
+    mat_dot(m.a1, m.a0, m.w1);
+    mat_sum(m.a1, m.b1);
+    mat_sig(m.a1);
+
+    mat_dot(m.a2, m.a1, m.w2);
+    mat_sum(m.a2, m.b2);
+    mat_sig(m.a2);
+   float y = *(m.a2.es); 
+  return y;
+}
 int main(void){
   srand(time(NULL));
-  Mat m = mat_alloc(3,3);
-  Mat n = mat_alloc(3,3);
-  mat_fill(m, 1);
-  mat_fill(n,2);
-  mat_sum(m, n);
-  mat_print(m);
-  return 0;
+  
+  Xor m;
+  m.a0 = mat_alloc(1,2);
+  m.w1 = mat_alloc(2,2);
+  m.b1 = mat_alloc(1,2);
+
+  m.a1 = mat_alloc(1,2);
+  m.w2 = mat_alloc(2,1);
+  m.b2 = mat_alloc(1,1);
+  m.a2 = mat_alloc(1,1);
+
+  mat_rand(m.w1, 0, 1); 
+  mat_rand(m.b1, 0, 1); 
+  mat_rand(m.w2, 0, 1); 
+  mat_rand(m.w2, 0, 1);
+
+  for(size_t i = 0 ; i  < 2 ; i++){
+    for(size_t j = 0 ; j < 2 ; j++){
+      printf("%zu  ^ %zu = %f\n",i,j,forward_xor(m,i,j));
+    } 
+  }
+ return 0;
 }
--- intermediate_stuff/nn.h
+++ intermediate_stuff/nn.h
@@ -19,6 +19,12 @@
 #define NN_PRINTF printf
 #endif
 
+#ifndef NN_MATH
+#include<math.h>
+#endif
+
+//#endif  //NN_H_
+
 //this is header part ... all declerations will be here
 typedef struct{
   size_t rows;
@@ -36,13 +42,18 @@ typedef struct{
 // */
 
 float rand_float(void);
+float sigmoidf(float x);
 void mat_fill(Mat m, float x);
 void mat_rand(Mat m, float low , float high);
 Mat mat_alloc(size_t rows, size_t cols);
 void mat_dot(Mat dst, Mat a, Mat b);
 void mat_sum(Mat dst , Mat a);
-void mat_print();
+void mat_print(Mat m, const char *name);
+void mat_sig(Mat m);
 
+#define MAT_PRINT(m) mat_print(m, #m)    //this very cool feature of c priprocessing.
+  //                                  //Where we can litterly convert input token into string. :))  
+  //  MAT_PRINT MACRO TAKES A TOKEN AND PROVIDE IT TO mat_print function and convert same token in string and pass as second arguement.
 
 
 #endif //NN_H
@@ -51,6 +62,13 @@ void mat_print();
 //defination of declerations will be here
 //use     #define NN_IMPLEMENTATION     TO USE definations in .c FILE.
 
+void mat_sig(Mat m){
+  for(size_t i = 0 ; i < m.rows ; i++){
+    for(size_t j = 0 ; j < m.cols ; j++){
+      MAT_AT(m,i,j) = sigmoidf(MAT_AT(m,i,j));
+    }
+  }
+}
 Mat mat_alloc(size_t rows, size_t cols){
   Mat m;
   m.rows = rows;
@@ -69,6 +87,10 @@ Mat mat_alloc(size_t rows, size_t cols){
  * */
 float rand_float(void){
   return (float)rand()/RAND_MAX;    //generates random number between [0,1]
+}
+
+float sigmoidf(float x){
+  return 1.f/(1.f + expf(-x)); 
 }
 
 void mat_fill(Mat m, float x){
@@ -91,12 +113,25 @@ void mat_rand(Mat m , float low , float high){   //to allocate matrix with some 
 
 
 void mat_dot(Mat dst, Mat a, Mat b){
-  (void) dst;
-  (void) a;
-  (void) b;
+  /*
+    MATRIX MULTIPLICATION IS NOT DEFINED IF A_mxn * B_pxq if n != p 
+    if m == p THEN IT'S DEFINED, AND RESULT OF THE MULTIPLICATION WILL BE
+    R_mxq
+    i.e. dst.rows == m && dis.cols = q;
+   */
+  NN_ASSERT(a.cols == b.rows);
+  NN_ASSERT(dst.rows == a.rows);
+  NN_ASSERT(dst.cols == b.cols);
+  size_t n = a.cols; 
+  for(size_t i = 0 ; i < dst.rows ; i++){
+    for(size_t j = 0 ; j < dst.cols ; j++){
+      MAT_AT(dst,i,j) = 0;
+      for(size_t k = 0 ; k < n ; k++){
+        MAT_AT(dst,i,j) +=  MAT_AT(a,i,k) * MAT_AT(b,k,j);
+      }       
+    } 
+  }
 }
-
-
 
 void mat_sum(Mat dst , Mat a){
   /*
@@ -115,10 +150,11 @@ void mat_sum(Mat dst , Mat a){
 }
 
 
-void mat_print(Mat m){
+void mat_print(Mat m, const char *name){
+  NN_PRINTF("%s = [\n", name);
   for(size_t i = 0 ; i < m.rows ; i++){
     for(size_t j = 0 ; j < m.cols ; j++){
-      NN_PRINTF("%f ",MAT_AT(m,i,j));   
+      NN_PRINTF("     %f ",MAT_AT(m,i,j));   
       //because actuall data stored as array but we interrpreting it as matrix.
       //for this I've written a micros MAT_AT 
       /*
@@ -129,6 +165,7 @@ void mat_print(Mat m){
     }
     NN_PRINTF("\n");
   }
+  NN_PRINTF("     ]\n");
 }
 
 
