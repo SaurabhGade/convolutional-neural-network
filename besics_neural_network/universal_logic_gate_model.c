81b06a4507f68009926dd75bea112ef7d99bb193
saurabh <saurabh@debian>
2024-07-04 22:53:51 +0530

    compleat all logic gates

--- besics_neural_network/universal_logic_gate_model.c
+++ besics_neural_network/universal_logic_gate_model.c
@@ -1,49 +1,49 @@
 #include<stdio.h>
-#include<math.h>
 #include<stdlib.h>
 #include<time.h>
-typedef float sample[3];
-sample or_train[] = {
-  {0, 0, 0},
-  {0, 1, 1},
-  {1, 0, 1},
-  {1, 1, 1}
-};
-sample nor_train[] = {
-  {0, 0, 1},
-  {0, 1, 0},
-  {1, 0, 0},
-  {1, 1, 0}
-};
-sample xor_train[] = {
+#include<math.h>
+//this architectrue will be two layer neural network.
+// (x ^ y) = (x|y) & ~(x&y)  = (x|y) & nand(x,y);
+//
+// first layer contain two neurons or , nand
+// second layer contain only one neural and.
+typedef float SAMPLE[3];
+SAMPLE xor_train[] = {
   {0, 0, 0},
   {0, 1, 1},
   {1, 0, 1},
   {1, 1, 0}
 };
-sample and_train[] = {
-  {0, 0, 0},
-  {0, 1, 0},
-  {1, 0, 0},
-  {1, 1, 1}
-};
-sample nand_train[] = {
-  {0, 0, 1},
-  {0, 1, 1},
-  {1, 0, 1},
-  {1, 1, 0}
-};
-sample xnor_train[] = {
+SAMPLE xnor_train[] = {
   {0, 0, 1},
   {0, 1, 0},
   {1, 0, 0},
   {1, 1, 1}
 };
-size_t train_count = 4;
-sample *train = xor_train;
 
+SAMPLE or_train[] = {
+  {0, 0, 0},
+  {0, 1, 1},
+  {1, 0, 1},
+  {1, 1, 1}
+};
+SAMPLE and_train[] = {
+  {0, 0, 0},
+  {0, 1, 0},
+  {1, 0, 0},
+  {1, 1, 1}
+};
+SAMPLE nand_train[] = {
+  {0, 0, 1},
+  {0, 1, 1},
+  {1, 0, 1},
+  {1, 1, 0}
+};
 
-typedef struct{
+const size_t train_count = 4;
+SAMPLE *train = xnor_train;
+
+typedef struct {
   float or_w1;
   float or_w2;
   float or_b;
@@ -58,41 +58,42 @@ typedef struct{
 
 } Xor;
 
-float sigmoidf(float x){    //map any value (+inf, -inf) between [0,1];
+
+float sigmoidf(float x){
   return 1.f/(1.f+expf(-x));
 }
+//we require function which feed given input in neural network.
+//this function is AI/ML word known as forwording functions.
+//this function will accept model and input values.
+float forward (Xor m, float x1 , float x2){
 
-float forwardf(Xor m , float x1 , float x2){
-  //we want values between 1 to 0 so using sigmoid fucntion.
-  float a = sigmoidf(m.or_w1*x1 + m.or_w2*x2 + m.or_b);
-  float b = sigmoidf(m.nand_w1*x1 + m.nand_w2*x2 + m.nand_b);
-  //a and b are intermidieat value for and this will feed to and.
-  float c = sigmoidf(a*m.and_w1 + b*m.and_w2 + m.and_b);
-  return c;
+  float y1 = sigmoidf(m.or_w1*x1 + m.or_w2*x2 + m.or_b);
+  float y2 = sigmoidf(m.nand_w1*x1 + m.nand_w2*x2 + m.nand_b);
+  //we got two intermidiate values. 
+  return sigmoidf(y1*m.and_w1 + y2*m.and_w2 + m.and_b);
 }
-
-float cost (Xor m){ float result = 0.0f;
+float cost(Xor m){
+  float result = 0.0f;
   for(size_t i = 0 ; i < train_count ; i++){
     float x1 = train[i][0];
     float x2 = train[i][1];
-    float y = forwardf(m, x1, x2);
+    float y = forward(m, x1, x2);
     float d = y - train[i][2];
     result += d*d;
   }
   result /= train_count;
   return result;
 }
+
 float rand_float(void){
-  return (float)rand()/RAND_MAX; 
+  return (float)rand()/(float)RAND_MAX;
 }
-
-
 Xor rand_xor(void){
   Xor m;
   m.or_w1 = rand_float();
   m.or_w2 = rand_float();
   m.or_b = rand_float();
-
+  
   m.nand_w1 = rand_float();
   m.nand_w2 = rand_float();
   m.nand_b = rand_float();
@@ -102,137 +103,118 @@ Xor rand_xor(void){
   m.and_b = rand_float();
   return m;
 }
+
 void print_xor(Xor m){
-  printf("or_w1 = %f\n", m.or_w1);
-  printf("or_w1 = %f\n", m.or_w2);
-  printf("or_b = %f\n\n", m.or_b);
+  printf("or_w1 = %f\n",m.or_w1); 
+  printf("or_w2 = %f\n",m.or_w2); 
+  printf("or_b = %f\n",m.or_b); 
 
-  printf("nand_w1 = %f\n", m.nand_w1);
-  printf("nand_w1 = %f\n", m.nand_w2);
-  printf("nand_b = %f\n\n", m.nand_b);
+  printf("nand_w1 = %f\n",m.nand_w1);
+  printf("nand_w2 = %f\n",m.nand_w2);
+  printf("nand_b = %f\n",m.nand_b);
 
-  printf("and_w1 = %f\n", m.and_w1);
-  printf("and_w1 = %f\n", m.and_w2);
-  printf("and_b = %f\n\n", m.and_b);
+  printf("and_w1 = %f\n",m.and_w1);
+  printf("and_w2 = %f\n",m.and_w2);
+  printf("and_b = %f\n",m.and_b);
 }
 
+//besically this function will calculate the derivative
+//now we have 3 neurons and 9 parameters so it is better
+//to make new function to find out finite difference.
 Xor finite_diff(Xor m, float eps){
-  Xor g; 
-  float c = cost(m);    //original cost.
+  Xor g;
   float saved;
+  float c = cost(m);
 
-  saved = m.or_w1;
-  m.or_w1 += eps;   //f(a+h)
-  g.or_w1 = (cost(m) - c)/eps;  //(f(a+h)-f(a))/h  //save result in g.
-  m.or_w1 = saved;  //restore original value.
-
+  saved = m.or_w1; 
+  m.or_w1 += eps;
+  g.or_w1 = (cost(m) - c)/eps;
+  m.or_w1 = saved;
+  //m.or_w1 -= eps; will also restore the value, But 
+  //we are working with floats and adding and substracting in float
+  //can introduce errors, and doing that multipale time accumlate errors.
+  //so it is better to save original value and then store it.
+  //in IEE754 arch. it is never recommended to frequently use add and sub with float.
   saved = m.or_w2;
   m.or_w2 += eps;
-  g.or_w2 = (cost(m)-c)/eps;
+  g.or_w2 = (cost(m) - c)/eps;
   m.or_w2 = saved;
 
   saved = m.or_b;
   m.or_b += eps;
-  g.or_b = (cost(m)-c)/eps;
+  g.or_b = (cost(m) - c)/eps;
   m.or_b = saved;
 
   saved = m.nand_w1;
-  m.nand_w1 += eps; 
-  g.nand_w1 = (cost(m)-c)/eps;
+  m.nand_w1 += eps;
+  g.nand_w1 = (cost(m) - c)/eps;
   m.nand_w1 = saved;
 
   saved = m.nand_w2;
   m.nand_w2 += eps;
-  g.nand_w2 = (cost(m)-c)/eps;
+  g.nand_w2 = (cost(m) - c)/eps;
   m.nand_w2 = saved;
 
-  saved =  m.nand_b;
+  saved = m.nand_b;
   m.nand_b += eps;
-  g.nand_b = (cost(m)-c)/eps;
+  g.nand_b = (cost(m) - c)/eps;
   m.nand_b = saved;
-
 
   saved = m.and_w1;
   m.and_w1 += eps;
-  g.and_w1 = (cost(m)-c)/eps;
+  g.and_w1 = (cost(m) - c)/eps;
   m.and_w1 = saved;
 
-  saved =  m.and_w2;
+  saved = m.and_w2;
   m.and_w2 += eps;
-  g.and_w2 = (cost(m)-c)/eps;
+  g.and_w2 = (cost(m) - c)/eps;
   m.and_w2 = saved;
 
   saved = m.and_b;
   m.and_b += eps;
-  g.and_b = (cost(m)-c)/eps;
+  g.and_b = (cost(m) - c)/eps;
   m.and_b = saved;
-  return g;
+ 
+ return g;
 }
+Xor learn(Xor m , Xor g , float rate){
+  m.or_w1 -= rate * g.or_w1;
+  m.or_w2 -= rate * g.or_w2;
+  m.or_b -= rate * g.or_b;
 
-Xor learn(Xor m, Xor g , float rate){   //besically apply the difference.
-  m.or_w1 -= rate*g.or_w1;  
-  m.or_w2 -= rate*g.or_w2;  
-  m.or_b -= rate*g.or_b;
+  m.nand_w1 -= rate * g.nand_w1;
+  m.nand_w2 -= rate * g.nand_w2;
+  m.nand_b -= rate * g.nand_b;
 
-  m.nand_w1 -= rate*g.nand_w1;
-  m.nand_w2 -= rate*g.nand_w2;
-  m.nand_b -= rate*g.nand_b;
-
-  m.and_w1 -= rate*g.and_w1;
-  m.and_w2 -= rate*g.and_w2;
-  m.and_b -= rate*g.and_b;
+  m.and_w1 -= rate * g.and_w1;
+  m.and_w2 -= rate * g.and_w2;
+  m.and_b -= rate * g.and_b;
   return m;
 }
-
-int main(void){
-  srand(time(0));
+unsigned int converge(float x){
+  if(x >= 0.5f)
+      return 1;
+  else return 0;
+}
+int main(){
+//  srand(69);
+  float eps = 1e-1;
+  float rate = 1e1;
   Xor m = rand_xor();
 
-  float eps = 1e-1;
-  float rate = 1e-1;
-
-  for(size_t i = 0 ; i < 1000*1000 ; i++){
-    Xor g = finite_diff(m , eps);
-    m = learn(m,g,rate);
-  //  printf("cost = %f\n",cost(m));
+  for(size_t i = 0 ; i < 1000*100 ; ++i){
+   // printf("%f\n",cost(m));
+    Xor g = finite_diff(m, eps);
+    //printf("%f\n",cost(m));
+    m = learn(m, g, rate);
   }
- // printf("-------------------------------------\n");
+
+  //test model
+
   for(size_t i = 0 ; i < 2 ; i++){
     for(size_t j = 0 ; j < 2 ; j++){
-      printf("%zu ^ %zu = %f\n",i,j,forwardf(m,i,j)); 
+      printf("%zu ^ %zu = %d\n",i,j,converge(forward(m, i, j)));
     }
   }
-
-
-  ///this gates can configure themself randomly so they may show random result
-  ///but combine result on train set will accurate this network works
-  ///on every gate just chage train pointer to the spcific dataSet,
-  ///i.e and_train, or_train ...
   return 0;
-
-
-
-  printf("------------------------------\n");
-  printf("\"AND\" neuron:\n"); 
-  for(size_t i = 0 ; i < 2 ; i++){
-    for(size_t j = 0 ; j < 2 ; j++){
-      printf("(%zu & %zu) = %f\n",i,j,sigmoidf(m.and_w1*i + m.and_w2*j + m.and_b));
-    }
-  }
-
-  printf("\"OR\" neuron:\n"); 
-  for(size_t i = 0 ; i < 2 ; i++){
-    for(size_t j = 0 ; j < 2 ; j++){
-      printf("(%zu | %zu) = %f\n",i,j,sigmoidf(m.or_w1*i + m.or_w2*j + m.or_b));
-
-    }
-  }
-  printf("\"NAND\" neuron:\n"); 
-  for(size_t i = 0 ; i < 2 ; i++){
-    for(size_t j = 0 ; j < 2 ; j++){
-      printf("~(%zu & %zu) = %f\n",i,j,sigmoidf(m.nand_w1*i + m.nand_w2*j + m.nand_b));
-    }
-  }
-
-  return 0; 
-  }
+}
